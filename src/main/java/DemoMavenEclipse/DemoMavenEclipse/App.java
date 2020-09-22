c63dc36fc6526dbf57f408d7c269492a412a9b64
gopal <gopal77namasivayam@gmail.com>
2020-09-22 15:47:39 +0530

    first

---
+++ src/main/java/DemoMavenEclipse/DemoMavenEclipse/App.java
@@ -0,0 +1,13 @@
+package DemoMavenEclipse.DemoMavenEclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/DemoMavenEclipse/DemoMavenEclipse/AppTest.java
@@ -0,0 +1,20 @@
+package DemoMavenEclipse.DemoMavenEclipse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
